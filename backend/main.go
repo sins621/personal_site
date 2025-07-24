acd463b4a2d25c31c1c5cd567b33dc55e6da1faa
sins <sinsmailza@gmail.com>
2025-07-24 23:58:59 +0200

    feat: add serving static files on go server

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main () {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	fs := http.FileServer(http.Dir("../frontend"))
+	http.Handle("/", fs)
+
+	port := ":8082"
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
